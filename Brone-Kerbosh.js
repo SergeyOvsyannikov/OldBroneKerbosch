8270ada21a15cc2d0d02d37667292a2e060a734c
SergeyOvsyannikov <turboman007@inbox.ru>
2020-11-12 22:02:06 +0300

    init

---
+++ Brone-Kerbosh.js
@@ -0,0 +1,154 @@
+//const { PerformanceObserver, performance } = require('perf_hooks');
+
+let counter = 0;
+let usualTime=[], sortTime=[];
+while(counter < 100){
+    let g = [
+        [0, 0, 0, 1, 0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+        [1, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+        [0, 1, 1, 0, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1, 1, 1, 0, 0]
+    ];
+
+    let graph = [
+        [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    ];
+    console.log("Исходный граф:", graph);
+    let cand = [];
+    let result = [];
+    for (let i = 0; i < graph.length; i++)
+        cand[i] = i;
+
+    function broneKerbosh(compsub = [], candidates = [], not = []) {
+        while (candidates.length && haveConnected(not, candidates)) {
+            let v = candidates[0];
+            compsub.push(v);
+            let newCandidates = [];
+            let newNot = [];
+
+            for (let i of candidates) {
+                if (!graph[i][v] && i !== v) {
+                    newCandidates.push(i);
+                }
+            }
+
+            for (let i of not) {
+                if (!graph[i][v] && i !== v) {
+                    newNot.push(i);
+                }
+            }
+
+            if (!newCandidates.length && !newNot.length) {
+                result.push([...compsub]);
+            } else {
+                broneKerbosh(compsub, newCandidates, newNot)
+            }
+            let indV = candidates.indexOf(v);
+            candidates.splice(indV, 1);
+            indV = compsub.indexOf(v);
+            compsub.splice(indV, 1);
+            not.push(v);
+        }
+    }
+
+    function haveConnected(not, candidates) {
+        let q;
+        for (let i of not) {
+            q = true;
+            for (let j of candidates) {
+                if (!!graph[i][j]) {
+                    q = false;
+                    break;
+                }
+            }
+            if (q)
+                return false;
+        }
+        return true;
+    }
+
+    let start = performance.now();
+    broneKerbosh([], cand, []);
+    let end = performance.now();
+    usualTime.push(end - start);
+    console.log("Время выполнения на обычных данных: ",end - start);
+    console.log("На обычных данных: ", result);
+    result.splice(0, result.length);
+    cand = [];
+
+    class Vertex {
+        constructor(index, weight) {
+            this.index = index;
+            this.weight = weight;
+        }
+    }
+
+    for (let i = 0; i < graph.length; i++){
+        let sum = 0;
+        for (let j = 0; j < graph[i].length; j++){
+            sum+=graph[i][j];
+        }
+        cand.push(new Vertex(i, sum));
+    }
+
+    let c = [];
+    cand.sort((a,b) => a.weight - b.weight);
+
+    for(let i = 0; i < cand.length; i++){
+        c.push(cand[i].index);
+    }
+
+    start = performance.now();
+    broneKerbosh([], c, []);
+    end = performance.now();
+    sortTime.push(end - start);
+    console.log("Время выполнения на отсортированных данных: ", end - start);
+    console.log("На отсортированных данных: ", result);
+    counter++;
+}
+
+console.log("Обычное время     Время на сортированных данных:");
+for(let i=0; i < usualTime.length; i++)
+{
+    console.log(usualTime[i], sortTime[i]);
+}
+
+let table = document.querySelector(".table__body");
+for(let i = 0; i < usualTime.length; i++){
+    table.innerHTML+=` <div class="table__row"><div>${usualTime[i]}</div><div>${sortTime[i]}</div></div>`;
+}
